cc466be62b6d64819fcae5c3b7b9060c1a441ac6
tharakaarpico <tharaka.c@arpicoinsurance.com>
2019-05-09 10:33:08 +0530

    ResultMethord Modifired

--- test/src/main/java/lk/exame/test/dao/ExameDAO.java
+++ test/src/main/java/lk/exame/test/dao/ExameDAO.java
@@ -8,7 +8,7 @@ import lk.exame.test.entity.ExameEntity;
 
 public interface ExameDAO extends CrudRepository<ExameEntity, Integer>{
 
-	List<ExameEntity>findByUserName(String userName);
+	List<ExameEntity>findByUserNameAndStatus(String userName,String status);
 	
 	ExameEntity findFirst1ByExameId(Integer exameId);
 
