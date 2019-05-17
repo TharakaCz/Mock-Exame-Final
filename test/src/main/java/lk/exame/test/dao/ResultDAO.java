d6a2284208eba49316ef601985357cc0f338b498
tharakaarpico <tharaka.c@arpicoinsurance.com>
2019-05-17 11:38:51 +0530

    Get Result by UserName

--- test/src/main/java/lk/exame/test/dao/ResultDAO.java
+++ test/src/main/java/lk/exame/test/dao/ResultDAO.java
@@ -12,7 +12,7 @@ import lk.exame.test.entity.ResultEntity;
  */
 public interface ResultDAO extends CrudRepository<ResultEntity, Integer>{
 
-	List<ResultEntity> findByUserName(String userName);
+	List<ResultEntity> findAllByUserName(String userName);
 	
 	ResultEntity findByResultId(Integer resultId);
 	
